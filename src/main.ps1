46c7276fc1bb3c58f085a434b61bf46dc214d8e2
Zic <git@zicstardust.com>
2026-05-21 10:53:43 -0400

    add SCHEDULE_INTERVAL

--- src/main.ps1
+++ src/main.ps1
@@ -5,13 +5,46 @@ $Arguments = @{
     Wait = $true
 }
 
+function Set-Interval {
+    param (
+        [Parameter(Mandatory)]
+        [string]$Interval,
+        [Parameter(Mandatory=$false)]
+        [Switch]$CheckError
+    )
+
+    if ($CheckError){
+        if((-not($Interval.Substring(0, $Interval.Length - 1) -match "^\d+$")) -Or (-not($Interval[-1] -match "[s|m|h|d|w]"))){
+            return $true
+        } else {
+            return $false
+        }
+    }
+
+    [char]$lastChar = $Interval[-1]
+    [Int64]$time = $Interval.Substring(0, $Interval.Length - 1)
+
+    if ($lastChar -Eq "s") {
+        return $time
+    } elseif ($lastChar -Eq "m") {
+        return $time*60
+    } elseif ($lastChar -Eq "h") {
+        return $time*3600
+    } elseif ($lastChar -Eq "d") {
+        return $time*86400
+    } elseif ($lastChar -Eq "w") {
+        return $time*604800
+    }
+}
+
+
+ if (Set-Interval -Interval $env:SCHEDULE_INTERVAL -CheckError){
+    Write-Host "invalid INTERVAL" -ForegroundColor Red
+    exit 1
+}
 
 while ($true){
-
-
-
     Start-Process @Arguments
-
-    Start-Sleep $SCHEDULE_INTERVAL
-
+    Start-Sleep -Seconds (Set-Interval -Interval $env:SCHEDULE_INTERVAL)
+    Write-Host "CONTAINER: Start interval $env:SCHEDULE_INTERVAL" -ForegroundColor Yellow
 }
